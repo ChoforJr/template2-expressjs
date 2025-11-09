9e6b99c18aa540e36ce0c769bd5f9946bc1882bf
ChoforJr <choforforsakang@gmail.com>
2025-11-09 05:56:05 +0100

    modified prisma config file

--- config/prisma.js
+++ config/prisma.js
@@ -1,5 +1,6 @@
 // This file creates and exports a single, reusable Prisma Client instance
 import { PrismaClient } from "../generated/prisma/client.ts";
+import "dotenv/config";
 
 const prisma = new PrismaClient();
 
